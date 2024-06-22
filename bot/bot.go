64191f8b206b878f052626282f75da80edade3a2
NoName13 <33531197+NNull13@users.noreply.github.com>
2024-06-21 23:57:29 -0300

    Beta 1.0.1

--- bot/bot.go
+++ bot/bot.go
@@ -128,8 +128,8 @@ func tokenQualityCheck(t TokenInfo) (string, bool) {
 	if config.TwitterFilter {
 		isEmpty := strings.TrimSpace(t.Data.Twitter) == ""
 		isInvalid := validate.Var(t.Data.Twitter, "url") != nil
-		isNotTwitter := strings.Contains(t.Data.Twitter, "twitter.com") || strings.Contains(t.Data.Twitter, "x.com")
-		if isEmpty || isInvalid || isNotTwitter {
+		isTwitter := strings.Contains(t.Data.Twitter, "twitter.com") || strings.Contains(t.Data.Twitter, "x.com")
+		if isEmpty || isInvalid || !isTwitter {
 			return getSymbol(false) + " TwitterFilter", false
 		}
 	}
@@ -137,8 +137,8 @@ func tokenQualityCheck(t TokenInfo) (string, bool) {
 	if config.TelegramFilter {
 		isEmpty := strings.TrimSpace(t.Data.Telegram) == ""
 		isInvalid := validate.Var(t.Data.Telegram, "url") != nil
-		isNotTelegram := strings.Contains(t.Data.Telegram, "telegram") || strings.Contains(t.Data.Telegram, "t.me")
-		if isEmpty || isInvalid || isNotTelegram {
+		isTelegram := strings.Contains(t.Data.Telegram, "telegram") || strings.Contains(t.Data.Telegram, "t.me")
+		if isEmpty || isInvalid || !isTelegram {
 			return getSymbol(false) + " TelegramFilter", false
 		}
 	}
